68b694ecec91aa77a2997592a19608cffd0a02c2
Radek Mucha <radek.mucha@aptitudesoftware.com>
2022-03-13 21:25:48 +0100

    initial commit

---
+++ generateCert.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+openssl req -nodes -x509 -newkey rsa:4096 -keyout localhost.key -out localhost.crt -sha256 -days 365 -subj "/C=PL/ST=DOL/O=Company/CN=localhost" -addext "subjectAltName=DNS:localhost"
+cat localhost.crt localhost.key > ./haproxy/localhost.pem
